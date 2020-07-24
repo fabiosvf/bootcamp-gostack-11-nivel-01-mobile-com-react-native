0c65690f47afe66bcad1cd7b6e35cb0489ed27e4
Fabio Silva Ferreira <fabiosvf@gmail.com>
2020-07-24 00:26:14 -0300

    Nível 01 - Mobile com React Native - Listando projetos da API - Instalando a lib axios e testando o componente ScrollView

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, ScrollView, Text, StyleSheet, StatusBar } from "react-native";
+
+import api from "./services/api";
 
 /**
  * Não possuem valor semântico (significado)
@@ -11,12 +13,24 @@ import { View, Text, StyleSheet, StatusBar } from "react-native";
  */
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        {projects.map((project) => (
+          <Text style={styles.project} key={project.id}>
+            {project.title}
+          </Text>
+        ))}
+      </ScrollView>
     </>
   );
 }
@@ -25,12 +39,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#7159c1",
-    justifyContent: "center",
-    alignItems: "center",
   },
-  title: {
+  project: {
     color: "#fff",
-    fontSize: 32,
-    fontWeight: "bold",
+    fontSize: 80,
   },
 });
