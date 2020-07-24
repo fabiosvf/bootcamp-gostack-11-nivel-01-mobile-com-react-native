9a76a9ac07acb3e777ce4954a501d742012abf6d
Fabio Silva Ferreira <fabiosvf@gmail.com>
2020-07-24 01:31:43 -0300

    Nível 01 - Mobile com React Native - Criando novos projetos

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {
   Text,
   StyleSheet,
   StatusBar,
+  TouchableOpacity,
 } from "react-native";
 
 import api from "./services/api";
@@ -31,6 +32,17 @@ export default function App() {
     });
   }, []);
 
+  async function handleAddProject() {
+    const response = await api.post("projects", {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Fabio Ferreira",
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
@@ -46,6 +58,14 @@ export default function App() {
             );
           }}
         />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -59,5 +79,17 @@ const styles = StyleSheet.create({
   project: {
     color: "#fff",
     fontSize: 30,
+  },
+  button: {
+    backgroundColor: "#fff",
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
